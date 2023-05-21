84ba7441e77cee275b368b5ec9170f5a9211668b
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-21 18:54:22 +0000

    added employee, shift, preference and login manager data models in models.py

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,73 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import login
 
 db = SQLAlchemy()
 
-class User(db.Model):
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return f'<User {self.email}>'
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+class Employee(db.Model):
+    __tablename__ = 'employees'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    employee_id = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    hire_date = db.Column(db.DateTime)
+    preferences = db.relationship('Preference', backref='employee', lazy='dynamic')
+    assigned_shift_id = db.Column(db.Integer, db.ForeignKey('shifts.id'))
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+class Shift(db.Model):
+    __tablename__ = 'shifts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    employees = db.relationship('Employee', backref='shift', lazy='dynamic')
+
+class Preference(db.Model):
+    __tablename__ = 'preferences'
+
+    id = db.Column(db.Integer, primary_key=True)
+    shift_id = db.Column(db.Integer, db.ForeignKey('shifts.id'))
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+    rank = db.Column(db.Integer)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Employee.query.get(int(user_id))
