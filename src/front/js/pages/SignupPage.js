2d076460fd0352be675b8105a241fe8199655d4a
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-02 22:07:24 +0000

    sign up form works now and redirects to login page

--- src/front/js/pages/SignupPage.js
+++ src/front/js/pages/SignupPage.js
@@ -1,17 +1,8 @@
-import React, { useState, Context } from "react";
-import {
-  Container,
-  Typography,
-  TextField,
-  Button,
-  Grid,
-} from "@material-ui/core";
+import React, { useState } from "react";
+import { Typography, TextField, Button, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: theme.spacing(8),
-  },
   form: {
     width: "100%",
     marginTop: theme.spacing(3),
@@ -21,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SignUpPage = () => {
+const SignupForm = () => {
   const classes = useStyles();
 
   const [formData, setFormData] = useState({
@@ -34,106 +25,81 @@ const SignUpPage = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const validateForm = () => {
-    const { password, confirmPassword } = formData;
-
-    if (password !== confirmPassword) {
-      return "Passwords do not match";
-    }
-
-    // Add more validation rules if needed
-
-    return null;
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const validationError = validateForm();
+    const { password, confirmPassword } = formData;
 
-    if (validationError) {
-      alert(validationError);
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
       return;
     }
 
-    // Call the registerUser action to handle the registration
-    actions
-      .registerUser(formData)
-      .then((success) => {
-        if (success) {
-          // Registration successful, redirect to the login page
-          window.location.href = "/login";
-        } else {
-          // Registration failed, display an error message
-          alert("Failed to register. Please try again.");
-        }
-      })
-      .catch((error) => {
-        console.error("Registration error:", error);
-        alert("An error occurred during registration. Please try again later.");
-      });
+    // Handle the registration logic here
+    // Example:
+    if (formData.email && formData.password) {
+      // Perform the registration logic
+      // Registration successful, redirect to the login page or show success message
+      window.location.href = "/";
+    } else {
+      // Registration failed, display an error message
+      alert("Failed to register. Please try again.");
+    }
   };
 
   return (
-    <Container component="main" maxWidth="xs" className={classes.container}>
-      <div>
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                value={formData.password}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="confirmPassword"
-                label="Confirm Password"
-                type="password"
-                id="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
             fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submitButton}
-          >
-            Sign Up
-          </Button>
-        </form>
-      </div>
-    </Container>
+            id="email"
+            label="Email Address"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+        </Grid>
+      </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submitButton}
+      >
+        Sign Up
+      </Button>
+    </form>
   );
 };
 
-export default SignUpPage;
+export default SignupForm;
