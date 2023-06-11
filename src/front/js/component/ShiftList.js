71cb1c180e86adea2a8cf39c8d3dbc591e4b78b3
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-11 20:47:20 +0000

    test

--- src/front/js/component/ShiftList.js
+++ src/front/js/component/ShiftList.js
@@ -1,43 +1,31 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import BidForm from "../pages/BidForm.js";
+import BidForm from "../pages/BidForm";
 
 const ShiftList = () => {
   const { store, actions } = useContext(Context);
-  const [shifts, setShifts] = useState([]);
   const [selectedShift, setSelectedShift] = useState(null);
 
   useEffect(() => {
-    const fetchShifts = async () => {
-      const result = await actions.fetchShifts();
-      if (result) {
-        setShifts(store.shifts);
-      } else {
-        // Handle error here
-        console.log(store.shiftError);
-      }
-    };
-    fetchShifts();
-  }, [store.shifts]);
+    actions.fetchShifts();
+  }, []);
 
   return (
-    <ErrorBoundary>
-      <div>
-        <h1>Available Shifts</h1>
-        <ul>
-          {shifts.map((shift) => (
-            <li key={shift.id}>
-              <h2>{shift.title}</h2>
-              <p>{shift.description}</p>
-              <p>Start Time: {shift.startTime}</p>
-              <p>End Time: {shift.endTime}</p>
-              <button onClick={() => setSelectedShift(shift)}>Bid</button>
-            </li>
-          ))}
-        </ul>
-        {selectedShift && <BidForm shift={selectedShift} />}
-      </div>
-    </ErrorBoundary>
+    <div>
+      <h1>Available Shifts</h1>
+      <ul>
+        {store.shifts.map((shift) => (
+          <li key={shift.id}>
+            <h2>{shift.title}</h2>
+            <p>{shift.description}</p>
+            <p>Start Time: {shift.startTime}</p>
+            <p>End Time: {shift.endTime}</p>
+            <button onClick={() => setSelectedShift(shift)}>Bid</button>
+          </li>
+        ))}
+      </ul>
+      {selectedShift && <BidForm shift={selectedShift} />}
+    </div>
   );
 };
 
