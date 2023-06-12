24b4a08134dc1a3a30967f45a0bd99a2b6bc8403
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-12 00:03:52 +0000

    modified the logic for generating random shifts for all the requirements necessary

--- src/front/js/component/ShiftSelect.js
+++ src/front/js/component/ShiftSelect.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 
 const generateShifts = (numTeams, peoplePerTeam) => {
   const shifts = [];
-  const teams = ["A", "B", "C", "D", "E", "F"]; // replace with your actual teams
-  const leads = ["John", "Jane", "Bob", "Alice", "Tom", "Jerry"]; // replace with your actual leads
+  const teams = ["A", "B", "C", "D", "E", "F"];
+  const leads = ["John", "Jane", "Bob", "Alice", "Tom", "Jerry"];
   const managers = [
     "Manager1",
     "Manager2",
@@ -12,19 +12,33 @@ const generateShifts = (numTeams, peoplePerTeam) => {
     "Manager4",
     "Manager5",
     "Manager6",
-  ]; // replace with your actual managers
-  const startTimes = ["08:00", "10:00", "12:00", "14:00", "16:00", "18:00"]; // example start times, adjust as necessary
-  const endTimes = ["16:00", "18:00", "20:00", "22:00", "00:00", "02:00"]; // example end times, adjust as necessary
+  ];
+  const startTimes = ["08:00", "10:00", "12:00", "14:00", "16:00", "18:00"];
+  const endTimes = ["16:00", "18:00", "20:00", "22:00", "00:00", "02:00"];
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]; // Days of the week
 
   for (let t = 0; t < numTeams; t++) {
+    const daysOff = [days[t], days[(t + 1) % 7]]; // Two consecutive days off for each team, wrapping around to the start of the week
     for (let p = 0; p < peoplePerTeam; p++) {
+      const dayOffPhone = days[(t + p + 2) % 7]; // Different day off from phone time for each member of a team
       const shift = {
-        id: t * peoplePerTeam + p + 1, // unique shift ID
+        id: t * peoplePerTeam + p + 1,
         team: teams[t],
         startTime: startTimes[t],
         endTime: endTimes[t],
         lead: leads[t],
         manager: managers[t],
+        daysOff: daysOff,
+        dayOffPhone: dayOffPhone,
       };
 
       shifts.push(shift);
@@ -35,7 +49,6 @@ const generateShifts = (numTeams, peoplePerTeam) => {
 };
 
 const ShiftSelect = ({ selectedShift, handleShiftChange }) => {
-  // Generate your shifts array here
   const shifts = generateShifts(6, 25);
 
   return (
@@ -46,7 +59,9 @@ const ShiftSelect = ({ selectedShift, handleShiftChange }) => {
         {shifts.map((shift) => (
           <option key={shift.id} value={shift.id}>
             {shift.team} ({shift.startTime} - {shift.endTime}, Lead:{" "}
-            {shift.lead}, Manager: {shift.manager})
+            {shift.lead}, Manager: {shift.manager}, Days Off:{" "}
+            {shift.daysOff.join(", ")}, Day Off from Phone Time:{" "}
+            {shift.dayOffPhone})
           </option>
         ))}
       </select>
